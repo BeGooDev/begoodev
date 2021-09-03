b3cfb4daf4d03892f8764dcb9b537f55c1e4776a
Philippe Gibert <philippe.gibert@gmail.com>
2021-09-03 09:04:54 +0200

    Add favicon

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,7 @@ function MyApp({Component, pageProps}) {
         <>
             <Head>
                 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+                <link rel="webmanifest" href="/site.webmanifest" />
             </Head>
             <Component {...pageProps} />
         </>
