ce0dbab149513ff65fe449dd6f9c8c65240601f2
Philippe Gibert <philippe.gibert@gmail.com>
2021-09-05 21:33:31 +0200

    update menu

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,36 +1,66 @@
 import Link from 'next/link';
+import classNames from "classnames";
 
-export default function Header() {
-    return (
-        <nav className="navbar is-primary is-fixed-top" role="navigation" aria-label="main navigation">
-            <div className="navbar-brand">
-                <a className="navbar-item" href="/">
-                    Philippe Gibert - BeGooDev
-                </a>
+import {Component} from 'react';
 
-                <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false"
-                   data-target="navbarBasicExample">
-                    <span aria-hidden="true"/>
-                    <span aria-hidden="true"/>
-                    <span aria-hidden="true"/>
-                </a>
-            </div>
 
-            <div id="navbarMain" className="navbar-menu">
-                <div className="navbar-start">
+class Header extends Component{
+    state = {
+        activeToggleNav: false
+    }
 
+    constructor(props) {
+        super(props);
+        this.toggleMenu = this.toggleMenu.bind(this)
+    }
+
+
+    /**
+     * Toggle menu
+     */
+    toggleMenu() {
+        this.setState(prevState => ({
+                activeToggleNav: !prevState.activeToggleNav
+        }))
+    }
+
+    render() {
+        const {activeToggleNav} = this.state;
+
+        return (
+            <nav className="navbar is-primary is-fixed-top" role="navigation" aria-label="main navigation">
+                <div className="navbar-brand">
+                    <a className="navbar-item" href="/">
+                        Philippe Gibert - BeGooDev
+                    </a>
+
+                    <a role="button" className={classNames('navbar-burger', {'is-active': activeToggleNav})}
+                       aria-label="menu" aria-expanded="false"
+                       data-target="navbarBasicExample" onClick={this.toggleMenu}>
+                        <span aria-hidden="true"/>
+                        <span aria-hidden="true"/>
+                        <span aria-hidden="true"/>
+                    </a>
                 </div>
 
-                <div className="navbar-end">
+                <div id="navbarMain" className={classNames('navbar-menu', {'is-active': activeToggleNav})}>
+                    <div className="navbar-start">
+
+                    </div>
+
+                    <div className="navbar-end">
 
 
-                    <Link href="/contact">
-                        <a className="navbar-item">
-                            Contact
-                        </a>
-                    </Link>
+                        <Link href="/contact">
+                            <a className="navbar-item">
+                                Contact
+                            </a>
+                        </Link>
+                    </div>
                 </div>
-            </div>
-        </nav>
-    )
+            </nav>
+        )
+    }
 }
+
+export default Header
